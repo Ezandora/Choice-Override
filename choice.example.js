ae6dae4ef7351501779ffbf3a4470376b263bb71
Ezandora <ezandora@gmail.com>
2020-11-24 11:47:24 -0600

    Update examples

--- choice.example.js
+++ choice.example.js
@@ -4,8 +4,8 @@ function main(page_text_encoded)
 	var choice_override_script = require("relay/choice.ash");
 	var page_text = choice_override_script.choiceOverrideDecodePageText(page_text_encoded);
 	
-	var form_fields = Lib.formFields(); //Delete this line if you don't need this.
-	var choice_id = choice_override_script.choiceOverrideDiscoverChoiceIDFromPageText(page_text); //Delete this line if you don't need this.
+	//var form_fields = Lib.formFields();
+	//var choice_id = choice_override_script.choiceOverrideDiscoverChoiceIDFromPageText(page_text);
 	
 	//Modify page_text as you will here.
 	Lib.write(page_text);
