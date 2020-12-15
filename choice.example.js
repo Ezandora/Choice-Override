ff747d68b744e7e8218c34e7595b0da7018bf405
Ezandora <ezandora@gmail.com>
2020-12-15 15:07:12 -0600

    Updated choice.example.js

--- choice.example.js
+++ choice.example.js
@@ -1,12 +1,17 @@
-function main(page_text_encoded)
+const kol = require("kolmafia")
+
+module.exports.main = function (page_text_encoded)
 {
 	//Standard inputs:
 	var choice_override_script = require("relay/choice.ash");
 	var page_text = choice_override_script.choiceOverrideDecodePageText(page_text_encoded);
-	
-	//var form_fields = Lib.formFields();
+	//Uncomment if you need these:
+	//var form_fields = kol.formFields();
 	//var choice_id = choice_override_script.choiceOverrideDiscoverChoiceIDFromPageText(page_text);
 	
+	
+	
+	
 	//Modify page_text as you will here.
-	Lib.write(page_text);
+	kol.write(page_text);
 }
